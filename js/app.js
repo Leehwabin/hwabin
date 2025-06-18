c642f17bf8e21cd530d3de44261dab26a75d2300
Leehwabin <hwabin2000@gmail.com>
2025-06-18 15:54:17 +0900

    up 0618

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+AOS.init();
 document.addEventListener("DOMContentLoaded",function(){
     const progressBar = document.querySelectorAll(".progress-bar");
     progressBar.forEach(bar => {
@@ -6,5 +6,20 @@ document.addEventListener("DOMContentLoaded",function(){
         setTimeout(() => {
             bar.style.width = target + "%";
         },500)
+    })
+
+    const searchInput = document.getElementById("searchInput");
+    const portfolioitems = document.querySelectorAll(".portfolio-item");
+
+    searchInput.addEventListener("input",function(){
+       const searchTerm = this.value.toLowerCase();
+       portfolioitems.forEach((item) => {
+            const title = item.querySelector("h4").textContent.toLowerCase();
+            if(title.includes(searchTerm)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+       })
     })
 })
